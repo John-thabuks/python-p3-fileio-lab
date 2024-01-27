1b2950e4163d1310b2712c601b703b544ebfd3ad
John-thabuks <thabuksjohn@gmail.com>
2024-01-27 14:04:16 +0300

    Assignment completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    convert_to_string = str(file_name)
+    f_name = Path(convert_to_string+".txt")
+    with open(f_name, encoding="UTF 8", mode="w") as new_file:
+        new_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    convert_to_str = str(file_name)
+    f_name = Path(convert_to_str+'.txt')
+    with open(f_name, encoding="UTF 8", mode="a") as append_file:
+        append_file.write(append_content)
+
+
 
 def read_file(file_name):
-    pass
+    convert_to_str= str(file_name)
+    f_name = Path(convert_to_str+".txt")
+    with open(f_name, encoding="UTF 8") as read_file:
+        content = read_file.read()
+        return content
